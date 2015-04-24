c00a6e4f9ef70ffada47c720f465aab455e87e66
Daniel Huie <huied93@uw.edu>
2015-04-24 10:53:28 -0700

    added all up-to-date front-end code, moved server.js to js folder

---
+++ js/signin.js
@@ -0,0 +1,24 @@
+﻿function signin() {
+    var userInput = [];
+    userInput[0] = $("#username").val();
+    userInput[1] = $("#password").val();
+
+    $.ajax({
+        type: "POST",
+        url: "admin.asmx/authenticate",
+        data: JSON.stringify({ login: userInput }),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {
+            var results = msg.d;
+            document.location.href = "http://localhost:2633/all-done.html";
+            //$("#results").text(results);
+            
+
+            
+        },
+        error: function (msg) {
+            console.log("Error");
+        }
+    });
+};

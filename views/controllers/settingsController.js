596d186f532042379aa387d74a42721132e5e0bd
Kyle Yuasa <yuasakyl@uw.edu>
2015-05-04 18:17:24 -0700

    Parsed csv file upon upload

--- views/controllers/settingsController.js
+++ views/controllers/settingsController.js
@@ -7,6 +7,19 @@ settingsController.controller('settingsCtrl', ['$scope', '$http',
 
   		console.log("Hello world from settings");
 
+  		$scope.upload = function() {
+  			var x = $("#data-input").get(0).files[0];
+  			var parsed = Papa.parse(x, {
+
+  				complete: function(results) {
+                alert(results);
+                alert(JSON.stringify(results));
+  			}
+  				
+            });
+  			
+  		}
+  		
 
 
 
